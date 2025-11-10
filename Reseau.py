04e3899277273d90cd37c8ebf045da2689cc5a09
Mathisadi <adinolfimathis@gmail.com>
2025-11-10 22:13:41 +0100

    Modif informations table

--- Reseau.py
+++ Reseau.py
@@ -11,25 +11,27 @@ class Element(Base):
     __tablename__ = "Element_reseau"
 
     id = Column(Integer, primary_key=True)
-    tension = Column(Integer)
-    etat = Column(Boolean)
+    type = Column(String, nullable=False)
+    tension = Column(Integer, nullable=False)
+    etat = Column(Boolean, nullable=False)
     voisin = Column(JSON)
-    topo = Column(String)
+    topo_poste = Column(String)
 
     def __repr__(self):
-        return f"<Element(id={self.id}, tension={self.tension}, etat={self.etat}, voisin={self.voisin}, topo={self.topo})>"
+        return f"<Element id={self.id} type={self.type} tension={self.tension} etat={self.etat} voisin={self.voisin} topo={self.topo_poste}>"
 
 # Création d'une session
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # Ajout d'un nouveau élément
-def add_element(tension, etat, voisin, topo):
-    nouvel_user = Element(tension=tension, etat=etat, voisin=voisin, topo=topo)
+def add_element(type, tension, etat, voisin, topo_poste):
+    nouvel_user = Element(type=type, tension=tension, etat=etat, voisin=voisin, topo_poste=topo_poste)
     session.add(nouvel_user)
     session.commit()
 
-"""Lecture des données
+"""
+Lecture des données
 ele = session.query(Element).all()
 for u in ele:
     print(u)

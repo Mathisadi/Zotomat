6d2ffda8ce0a384c1f8d2dd6f7ec58bba8eb01e4
Mathisadi <adinolfimathis@gmail.com>
2025-11-11 19:29:10 +0100

    Sauvegarde

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-"L objectif est de connaitre la topo du réseau et en fonction on réajuste les automates"
+import pandas as pd
+import numpy as np
 
-"Pour savoir si c est une antenne c est un poste ou il y a qu une ligne ou plusieurs ligne mais qu une pas en antente"
+# Ouverture de la BDD
+data = pd.read_sql("Element_reseau", "sqlite:///RSO.db", index_col="id")
 
-def antenne(element_poste):
-    "On trouve les liens connecté au poste"
-    voisin_id = element_poste.voisin
+# On cree un dataframe de pandas
+data = pd.DataFrame(data)
+
+print(data)
+
+# On cherche les élements avec le bon id
+data = data.iloc[0]
+
+# On printe le dataframe
+print(data)
+
+# Comment vérifier la topo de tous les postes, on commence par les postes qui n'ont qu'un seul voisin
+# Puis on remonte dans ces voisins jusqu'à ceux qu'un poste n'est plus un seul voisin ou des antennes
+
+# Fonction qui définit les postes n'ayant qu'une ligne ES comme un poste en antenne
+def postes_1_voisin(id_poste, df=data):
+    # On convertit le voisin en array numpy
+    id_voisin = np.array(df.iloc[id_poste]["voisin"])
     
-    "Parmis ceux qui sont des voisins on regarde si ce sont des antennes et en service"
+    # Les ID = position dans la dataframe + 1
+    id_voisin = id_voisin - 1
     
+    # On vérifie que dans tous ces voisins on à qu'une ligne ES
+    
+    
+    # On garde que les vosins qui sont des lignes
     

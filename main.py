1d915a47efe2970ddcbd4e17a6b79935796784c2
Mathisadi <adinolfimathis@gmail.com>
2025-11-11 19:36:42 +0100

    Fonction qui teste si un poste à un seul voisin

--- main.py
+++ main.py
@@ -9,25 +9,20 @@ data = pd.DataFrame(data)
 
 print(data)
 
-# On cherche les élements avec le bon id
-data = data.iloc[0]
-
-# On printe le dataframe
-print(data)
-
 # Comment vérifier la topo de tous les postes, on commence par les postes qui n'ont qu'un seul voisin
 # Puis on remonte dans ces voisins jusqu'à ceux qu'un poste n'est plus un seul voisin ou des antennes
 
 # Fonction qui définit les postes n'ayant qu'une ligne ES comme un poste en antenne
-def postes_1_voisin(id_poste, df=data):
+def poste_1_voisin(id_poste, df=data):
     # On convertit le voisin en array numpy
-    id_voisin = np.array(df.iloc[id_poste]["voisin"])
+    id_voisin = np.array(df.iloc[id_poste - 1]["voisin"])
     
     # Les ID = position dans la dataframe + 1
     id_voisin = id_voisin - 1
     
     # On vérifie que dans tous ces voisins on à qu'une ligne ES
+    df_voisin = df.iloc[id_voisin]
+
+    df_voisin = df_voisin[(df_voisin["type"] == "Ligne") & (df_voisin["etat"] == "ES")]
     
-    
-    # On garde que les vosins qui sont des lignes
-    
+    return len(df_voisin) <= 1
